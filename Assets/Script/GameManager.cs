8ebf50c35505de8b17d5acb21e37b14fc41dee8b
Loky <lokyseo0434@nate.com>
2024-03-31 21:39:39 +0900

    animationFix, Difficulty, AimUI

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Text spawnTime;
-    
+    public GameObject esc_menu;
+
+
     void Start()
     {
            
@@ -14,6 +15,22 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(esc_menu.gameObject.activeSelf)
+            {
+                esc_menu.SetActive(false);
+            }
+            else
+            {
+                esc_menu.SetActive(true);
+            }
+        }
+    }
+
+
+    public void BackToLobby()
+    {
+        SceneManager.LoadScene("Lobby");
     }
 }
--- Assets/Script/Player_Shot.cs
+++ Assets/Script/Player_Shot.cs
@@ -6,18 +6,29 @@ using UnityEngine.UI;
 
 public class Player_Shot : MonoBehaviour
 {
+
+    [Header("이미지")]
     public Image hit_Image;
+    public Image zoom_Image;
+    public Image zoom_x4Image;
     public Text textbulletCount;
 
-    public ParticleSystem gunFire;
+    [Header("게임 오브젝트")]
+    public GameObject spawnSphere;
+    public GameObject sphere;
     public GameObject Gun;
+
+    public ParticleSystem gunFire;
     Animator fire_anim;
 
     int curBulletCount;
     int maxBulletCount;
 
+    int weaponType; //권총 : 0, AR : 1, SR : 2
+
     void Start()
     {
+        weaponType = 0;
         maxBulletCount = 6;
         curBulletCount = maxBulletCount;
         fire_anim = Gun.GetComponent<Animator>();
@@ -25,7 +36,7 @@ public class Player_Shot : MonoBehaviour
 
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward* 100, Color.red);
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.red);
         if (Input.GetKeyDown(KeyCode.Mouse0) && curBulletCount > 0)
         {
             curBulletCount--;
@@ -38,9 +49,9 @@ public class Player_Shot : MonoBehaviour
 
             if (Physics.Raycast(ray, out hitData))
             {
-                if(hitData.transform.name == "Start_Button") 
+                if (hitData.transform.name == "Start_Button")
                 {
-                    if(hitData.transform.GetComponent<Spawn_Sphere>().isReady)//종료함
+                    if (hitData.transform.GetComponent<Spawn_Sphere>().isReady)//종료함
                     {
                         hitData.transform.GetComponent<Spawn_Sphere>().isReady = false;
                         hitData.transform.GetComponent<Spawn_Sphere>().isSpawnStart = false;
@@ -56,58 +67,83 @@ public class Player_Shot : MonoBehaviour
                     }
                 }
 
-                if(hitData.transform.tag == "Target")
+                if (hitData.transform.tag == "LevelTrigger")
+                {
+                    hitData.transform.GetComponent<Renderer>().material.color = Color.black;
+
+                    switch (hitData.transform.name)
+                    {
+                        case "Easy":
+                            spawnSphere.GetComponent<Spawn_Sphere>().spawnLevel = 2.0f;
+                            sphere.GetComponent<Sphere_Trigger>().destroyLevel = 1.8f;
+                            break;
+
+                        case "Normal":
+                            spawnSphere.GetComponent<Spawn_Sphere>().spawnLevel = 1.5f;
+                            sphere.GetComponent<Sphere_Trigger>().destroyLevel = 1.3f;
+                            break;
+
+                        case "Difficult":
+                            spawnSphere.GetComponent<Spawn_Sphere>().spawnLevel = 1.2f;
+                            sphere.GetComponent<Sphere_Trigger>().destroyLevel = 1.0f;
+                            break;
+
+                    }
+
+
+                }
+
+                if (hitData.transform.tag == "Target")
                 {
                     hit_Image.color = new Color(1, 1, 1, 1);
                 }
 
-                if(hitData.transform.name.Contains("Sphere"))
+                if (hitData.transform.name.Contains("Sphere"))
                 {
                     Destroy(hitData.transform.gameObject);
-                    Spawn_Sphere.count_Score++;
+                    spawnSphere.GetComponent<Spawn_Sphere>().count_Score++;
                 }
-    
+
             }
         }
 
-       if(Input.GetKeyDown(KeyCode.R))
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if(weaponType != 2)
+            {
+                if (zoom_Image.gameObject.activeSelf)
+                {
+                    zoom_Image.gameObject.SetActive(false);
+                    this.transform.position += new Vector3(0, 0, -1);
+                }
+                else
+                {
+                    zoom_Image.gameObject.SetActive(true);
+                    this.transform.position += new Vector3(0, 0, 1);
+                }
+            }
+            else
+            {
+                if (zoom_x4Image.gameObject.activeSelf)
+                {
+                    zoom_x4Image.gameObject.SetActive(false);
+                    this.transform.position += new Vector3(0, 0, -8);
+                }
+                else
+                {
+                    zoom_x4Image.gameObject.SetActive(true);
+                    this.transform.position += new Vector3(0, 0, 8);
+                }
+            }
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
         {
             curBulletCount = maxBulletCount;
             textbulletCount.text = curBulletCount + " / " + maxBulletCount;
 
         }
     }
-    /*
-    void Fire()
-    {
-        aimControl.Fire();
-        if (fireTime >= fireRate) // 연사속도 시간을 넘으면
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(Camera.main.transform.position,  // 에임 정확도에 따라 레이를 쏨
-                Camera.main.transform.forward + // 현재 카메라가 보는 방향을 시작점으로 에임의 정확도 범위 중 랜덤한 위치로 발사
-                new Vector3(Random.Range(-aimControl.accuracy, aimControl.accuracy), Random.Range(-aimControl.accuracy, aimControl.accuracy), 0f),
-                out hit, range))
-            {
-                if (hit.transform.tag == "Head") // 머리에 맞으면 2배의 데미지
-                {
-                    BotControl bot = hit.transform.gameObject.GetComponent<BotControl>();
-                    bot.Damaged(10);
-                }
-                else if (hit.transform.gameObject.tag == "Bot") // 몸통일 경우 일반 데미지
-                {
-                    BotControl bot = hit.transform.gameObject.GetComponent<BotControl>();
-                    bot.Damaged(5);
-                }
-            }
-            Instantiate(bulletEffect, hit.point * 1.001f, Quaternion.LookRotation(hit.normal)); // 총 맞은 위치 표현
-            GameObject effect_L = Instantiate(shootEffect, shootPoint_L); // 좌우 총 이펙트
-            GameObject effect_R = Instantiate(shootEffect, shootPoint_R);
 
-            Destroy(effect_L, 0.1f); Destroy(effect_R, 0.1f); // 이펙트 삭제
-            currentBullet--; // 총알 감소
-            fireTime = 0f;
-        }
-    }
-    */
 }
